cd2fc2e1406939caeeccac8d7ae2126d93ce9451
Charith Ranasinghe <charithfyp@gmail.com>
2018-07-22 23:19:28 +0530

    An example class for how to implement lazy loading

---
+++ Lazy-Loading/Lazy-Loading/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lazy_Loading
+{
+    class Program
+    {
+
+        public class Employee
+        {
+           
+
+            public int EmployeeID { get; set; }
+            public string EmployeeName { get; set; }
+            public List<string> EarlyDesignations = null;
+
+            public List<string> deignations
+            {
+                get
+                {
+                    if(EarlyDesignations == null)
+                    {
+                        EarlyDesignations = GetEarlyDesignations(this.EmployeeID);
+                    }
+                    return EarlyDesignations;
+                }
+            }
+            
+
+            public Employee(int employeeID, string employeeName)
+            {
+                EmployeeID = employeeID;
+                EmployeeName = employeeName;
+               
+            }
+
+            public List<string> GetEarlyDesignations(int empID)
+            {
+                List<string> designation = new List<string>();
+                designation.Add("Intern");
+                designation.Add("Trainee Software Engineer");
+                designation.Add("Software Engineer");
+
+                return designation;
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Employee obj = new Employee(1,"Charith");
+
+            foreach(var a in obj.EarlyDesignations)
+            {
+                Console.WriteLine(a);
+                Console.ReadLine();
+            }
+        }
+    }
+}
